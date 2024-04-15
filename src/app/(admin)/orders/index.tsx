7b5db8e2104401455021f0cd59171de690f757fe
ArinzeJeffrey-droid <nnamenearinze@gmail.com>
2024-04-15 18:15:41 +0100

    Add orders screen for admin side.

---
+++ src/app/(admin)/orders/index.tsx
@@ -0,0 +1,17 @@
+import orders from "@/assets/data/orders";
+import OrderItemListItem from "@/src/components/OrderListItem";
+import { Stack } from "expo-router";
+import { FlatList } from "react-native";
+
+export default function MenuScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Orders" }} />
+      <FlatList
+        data={orders}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        renderItem={({ item }) => <OrderItemListItem order={item} />}
+      />
+    </>
+  );
+}
