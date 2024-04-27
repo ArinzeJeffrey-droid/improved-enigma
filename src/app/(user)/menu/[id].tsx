307dd503508f570874a79fff9b87c55a84ad3e21
ArinzeJeffrey-droid <nnamenearinze@gmail.com>
2024-04-27 22:56:14 +0100

    Read product by ID.

--- src/app/(user)/menu/[id].tsx
+++ src/app/(user)/menu/[id].tsx
@@ -1,21 +1,22 @@
-import products from "@/assets/data/products";
+import { useProduct } from "@/src/api/products";
 import Button from "@/src/components/Button";
 import { defaultImage } from "@/src/components/ProductListItem";
 import { useCart } from "@/src/providers/CartProvider";
 import type { PizzaSize } from "@/src/types";
 import { Stack, useLocalSearchParams, useRouter } from "expo-router";
 import { useState } from "react";
-import { StyleSheet, View, Text, Image, Pressable } from "react-native";
+import { StyleSheet, View, Text, Image, Pressable, ActivityIndicator } from "react-native";
 
 const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailScreen = () => {
-  const { id } = useLocalSearchParams();
-  const product = products.find((p) => p.id.toString() === id);
-  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
-  const { addItem } = useCart();
-
   const router = useRouter();
+  const { addItem } = useCart();
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
+
+  const { id: idString } = useLocalSearchParams();
+  const id = parseFloat(typeof idString === "string" ? idString : idString[0]);
+  const { data: product, error, isLoading } = useProduct(id);
 
   const addToCart = () => {
     if (!product) return;
@@ -23,7 +24,9 @@ const ProductDetailScreen = () => {
     router.push("/cart");
   };
 
-  if (!product) return <Text>Product not found</Text>;
+  if (isLoading) return <ActivityIndicator />;
+
+  if(error) return <Text>Failed to fetch products</Text>
 
   return (
     <View style={styles.container}>
