b6b6f29803c2cc18d4a52b5bb1c8b2cd7f3e5eba
ArinzeJeffrey-droid <nnamenearinze@gmail.com>
2024-04-27 21:00:13 +0100

    Supabase setup.

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = "https://fieursatnxosirmljbnj.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpZXVyc2F0bnhvc2lybWxqYm5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQyNDc3NDYsImV4cCI6MjAyOTgyMzc0Nn0.Zpw7mHrcXreLQVcG5CbXiwYZPERfMYi0lO5-ptixcLs";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
